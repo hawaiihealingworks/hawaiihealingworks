7c56c16155339727b315bdf9250cb9352fff8627
HawaiiHealingWorks <matthew@hawaiihealingworks.com>
2013-02-08 17:55:15 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hawaiihealingworks::Application.config.secret_token = '9a49797bc5af202e0a1204172a40bf48dbff28a2a4b1d08dd73abb5fbc1cf09d8cd3e5705b4f2430827d58f13a49c17d75d03b56dfcf7d546615dd90ec1a6b71'
